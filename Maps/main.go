83d136cafd18b56c1783bebb19e181b00e23339b
Sagar Hande <sagar.hande.work@gmail.com>
2022-11-30 15:53:11 +0530

    Some changes in maps

--- Maps/main.go
+++ Maps/main.go
@@ -7,25 +7,11 @@ import (
 func main() {
 	// 1st way to declare map
 
-	// colors := map[string]string{
-	// 	"red":   "#FF0000",
-	// 	"white": "#FFFFFF",
-	// }
-
-	// 2nd way to declare map
-
-	//var colors map[string]string  // this will create a nil map i.e. a reference pointing to Nil  DON't USE THIS, USE BELOW
-
-	colors := map[string]string{}
-
-	// 3rd way to declare map
-
-	// colors := make(map[string]string)
-
-	// ADD VALUES TO MAP SAME AS Dictionary in PYTHON
-
-	colors["red"] = "#FF0000"
-	colors["white"] = "#FFFFFF"
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"white": "#FFFFFF",
+		"green": "#00FF00",
+	}
 
 	fmt.Println(colors)
 
